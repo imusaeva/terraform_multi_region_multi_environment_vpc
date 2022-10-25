c01760bf4ce7fbc0974c5b5ef7d8c51c2a216f40
EC2 Default User <ec2-user@ip-172-31-26-246.us-west-2.compute.internal>
2022-10-25 23:09:39 +0000

    FileStructure to create VPCs in multiple regions

---
+++ FileStructure/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "aws-session-may2022-remote-backend"
+    key    = "filestructure/qa/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-may2022-state-lock-table" # statelocking can be done by Dynamodb table on AWS. 
+  }
+}
